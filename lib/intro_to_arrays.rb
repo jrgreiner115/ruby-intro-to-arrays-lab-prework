c23c02e67ed69e77660d4366a0f9a169f43881c2
Joe Greiner <jrgreiner115@gmail.com>
2018-03-22 15:15:24 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  ["chili's", "applebee's"]
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+first_element_with_array_methods(array)
+i = 0
+array[i]
+end
